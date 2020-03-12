a77fc123c4c23d02b022d7a3b2c5ba7b7b64b1e3
sanmiankunma <kunma3@outlook.com>
2020-03-12 21:05:15 +0800

    Site updated: 2020-03-12 21:05:11

---
+++ js/gallery.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    if (typeof ($.fn.lightGallery) === 'function') {
+        $('.article').lightGallery({ selector: '.gallery-item', closable: true, download: false, hideBarsDelay: 1500, actualSize: false, showThumbByDefault: false });
+    }
+    if (typeof ($.fn.justifiedGallery) === 'function') {
+        if ($('.justified-gallery > p > .gallery-item').length) {
+            $('.justified-gallery > p > .gallery-item').unwrap();
+        }
+        $('.justified-gallery').justifiedGallery({ margins: 3, captions: false });
+    }
+});
